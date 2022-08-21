ec09f56c3cf4569e0f750e81cc9c4776db425250
Christian Boyle <mail@christianboyle.com>
2022-08-21 13:34:57 -0500

    add 06-useCallback

---
+++ 06-useCallback/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useCallback } from 'react'
+import List from './List.jsx'
+
+export default function App() {
+  const [number, setNumber] = useState(1)
+  const [dark, setDark] = useState(false)
+
+  const getItems = useCallback(() => {
+    return [number, number + 1, number + 2]
+  }, [number])
+
+  const theme = {
+    backgroundColor: dark ? '#333' : '#fff',
+    color: dark ? '#fff' : '#333'
+  }
+
+  return (
+    <div style={theme}>
+      <input
+        type='number'
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Toggle theme
+      </button>
+      <List getItems={getItems} />
+    </div>
+  )
+}

98659c3a2905834794c1aeeae619759665c9e4a1
Christian Boyle <mail@christianboyle.com>
2022-08-22 14:46:12 -0500

    add 07-useReducer

---
+++ 07-useReducer/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useReducer } from 'react'
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 }
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+  function increment() {
+    dispatch({ type: ACTIONS.INCREMENT })
+  }
+
+  function decrement() {
+    dispatch({ type: ACTIONS.DECREMENT })
+  }
+
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </>
+  )
+}

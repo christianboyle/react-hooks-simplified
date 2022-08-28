a35b1f25554ccc81dc1763b8926f83a5062d4eb2
Christian Boyle <mail@christianboyle.com>
2022-08-28 10:03:46 -0500

    add 12-useImperativeHandle

---
+++ 12-useImperativeHandle/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useRef } from 'react'
+import ConfirmationModal from './ConfirmationModal'
+
+function App() {
+  const [open, setOpen] = useState(false)
+  const modalRef = useRef()
+
+  return (
+    <>
+      <button onClick={() => setOpen(true)}>Open</button>
+      <button onClick={() => modalRef.current.focusCloseBtn()}>
+        Focus Close
+      </button>
+      <button onClick={() => modalRef.current.focusConfirmBtn()}>
+        Focus Confirm
+      </button>
+      <button onClick={() => modalRef.current.focusDenyBtn()}>
+        Focus Deny
+      </button>
+      <ConfirmationModal
+        ref={modalRef}
+        isOpen={open}
+        onClose={() => setOpen(false)}
+      />
+    </>
+  )
+}
+
+export default App

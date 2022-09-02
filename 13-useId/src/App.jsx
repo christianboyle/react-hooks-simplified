434288630d18f01c266f211af092e1e8b49ab52d
Christian Boyle <mail@christianboyle.com>
2022-09-02 08:48:46 -0500

    add 13-useId

---
+++ 13-useId/src/App.jsx
@@ -0,0 +1,17 @@
+import EmailForm from './EmailForm'
+
+function App() {
+  return (
+    <>
+      <EmailForm />
+      <article style={{ marginBlock: '1rem' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+        voluptatum blanditiis velit ullam eos quia officia ipsa iste. In quidem
+        asperiores rem a repudiandae sit porro exercitationem ut, quis itaque.
+      </article>
+      <EmailForm />
+    </>
+  )
+}
+
+export default App
---
+++ 13-useId/src/EmailForm.jsx
@@ -0,0 +1,17 @@
+import { useId } from 'react'
+
+function EmailForm() {
+  const id = useId()
+
+  return (
+    <>
+      <label htmlFor={`${id}-email`}>Email</label>
+      <input id={`${id}-email`} type='email' />
+      <br />
+      <label htmlFor={`${id}-name`}>Name</label>
+      <input id={`${id}-name`} type='text' />
+    </>
+  )
+}
+
+export default EmailForm

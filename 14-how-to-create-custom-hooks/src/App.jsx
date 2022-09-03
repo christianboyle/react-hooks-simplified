a05e279be926ab26e8ec840b61237903dd2312e9
Christian Boyle <mail@christianboyle.com>
2022-09-03 12:35:08 -0500

    add 14-how-to-create-custom-hooks

---
+++ 14-how-to-create-custom-hooks/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import useLocalStorage from './useLocalStorage'
+import useUpdateLogger from './useUpdateLogger'
+
+function App() {
+  const [name, setName] = useLocalStorage('name', '')
+  useUpdateLogger(name)
+  return (
+    <input
+      type='text'
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    ></input>
+  )
+}
+
+export default App
